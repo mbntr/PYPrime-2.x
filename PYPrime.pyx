03ee93d3aed255c264c315a9026f9663653b22a2
Jaakko Paso <potto558@gmail.com>
2021-05-18 15:27:11 +0200

    PYPrime 2.0, Build 210127

--- PYPrime.pyx
+++ PYPrime.pyx
@@ -21,13 +21,6 @@ kernel32 = WinDLL('kernel32', use_last_error=True)
 # Globals
 cdef ull pr = 0
 cdef ull vr = -1
-runs = 7
-results = []
-corr_results = []
-primes = []
-
-pr = 2048000000
-vr = 2047999957
 
 # Header
 
@@ -44,19 +37,27 @@ class Header:
             hpr //= 1000
             ppr = "B"
     
-        print(f"{85 * '-'}\n{35 * ' '}PYPrime 2.2{35 * ' '}\n{85 * '-'}\n\n" 
-              f'OS    : {self.OS}\n' 
-              f'Timer : {round(self.qpf / 1000000, 2)} MHz\n'
+        print(f"{85 * '-'}\n{35 * ' '}PYPrime 2.0{35 * ' '}\n{85 * '-'}\n\n" \
+              f'OS    : {self.OS}\n' \
+              f'Timer : {round(self.qpf / 1000000, 2)} MHz\n' \
               f'Prime : {hpr}{ppr} - up to {pr:n}\n', flush=True)
 
 # Score
 
 class Score:   
-    def __init__(self, time):
+    def __init__(self, prime, valid, time):
+        self.prime = prime
+        self.valid = valid
         self.time = time
 
     def output(self):
-            print(f"\nAverage computation time : {round(self.time, 3):0.3f} s")
+        if not self.valid:
+            print(f"\nINVALID: {self.prime}", flush=True)
+        else:
+            line1 = f"Prime number     : {self.prime:n} is VALID"
+            line2 = f"Computation time : {round(self.time, 3):0.3f} s"
+
+            print("\n".join(["", line1, line2]), flush=True)
 
 
 # Benchmark
@@ -79,8 +80,8 @@ cdef print_status(int loop, ull qpf, ull start_time):
     end_time = wintypes.LARGE_INTEGER()
 
     kernel32.QueryPerformanceCounter(byref(end_time))
-    
-    print(f"|{'█'* (loop * 2) + ' '* (18 - loop * 2)}| step {loop} --- {round((end_time.value - start_time) / qpf, 3))} s", end="\r")
+
+    print("    Step {:} ....... {:0.3f} s".format(loop, round((end_time.value - start_time) / qpf, 3)), flush=True)
 
 cdef ull calc(unsigned char [::1] sieve, ull limit, ull sqrtlimit, ull qpf, ull start_time) nogil:
     cdef ull limit1, sqrtlimit1, loopstep, nextstep, x, x2, x2b3, x2b4, y, y2, n, m, o, nd, md
@@ -160,21 +161,20 @@ cdef ull calc(unsigned char [::1] sieve, ull limit, ull sqrtlimit, ull qpf, ull 
     return x
 
 cdef benchmark(ull limit, ull qpf):
+    cdef ull resultx, result
+    cdef ull sieve_len = (limit // 8) + 1
 
     start_time = wintypes.LARGE_INTEGER()
     end_time = wintypes.LARGE_INTEGER()
 
-
-    cdef ull resultx, result
-    cdef ull sieve_len = (limit // 8) + 1
     # Memory allocation
-    # print_memalloc(sieve_len)
-    
-    
+    print_memalloc(sieve_len)
+
     sieve_data = cvarray(shape=(sieve_len,), itemsize=sizeof(unsigned char), format="B")
     cdef unsigned char[::1] sieve = sieve_data
 
-    
+    print("Starting benchmark:\n", flush=True)
+
     # Start timestamp
     kernel32.QueryPerformanceCounter(byref(start_time))
 
@@ -188,6 +188,8 @@ cdef benchmark(ull limit, ull qpf):
     result = (sieve[resultx].bit_length() - 1) + resultx * 8
     time = round((end_time.value - start_time.value) / qpf, 3)
 
+    # Output end time
+    print("    Sieve Scan ... {:0.3f} s".format(time), flush=True)
 
     return [ result, result == vr, time ]
 
@@ -196,74 +198,76 @@ cdef benchmark(ull limit, ull qpf):
 
 while True:
     # Command line parameters
-    for i in sys.argv[1:]:
-    
-        if i.upper() == "32M":
+    for i in sys.argv:
+        if i == "1M":
+            pr = 1000000
+            vr = 999983
+            break
+        if i == "2M":
+            pr = 2000000
+            vr = 1999993
+            break
+        if i == "4M":
+            pr = 4000000
+            vr = 3999971
+            break
+        if i == "8M":
+            pr = 8000000
+            vr = 7999993
+            break
+        if i == "16M":
+            pr = 20000000
+            vr = 19999999
+            break
+        if i == "32M":
             pr = 32000000
             vr = 31999939
             break
-            
-        if i.upper() == "64M":
+        if i == "64M":
             pr = 64000000
             vr = 63999979
             break
-            
-        if i.upper() == "128M":
+        if i == "128M":
             pr = 128000000
             vr = 127999981
             break
-            
-        if i.upper() == "256M":
+        if i == "256M":
             pr = 256000000
             vr = 255999983
             break
-            
-        if i.upper() == "512M":
+        if i == "512M":
             pr = 512000000
             vr = 511999979
             break
-            
-        if i.upper() == "1024M" or i.upper() == "1B":
+        if i == "1024M" or i == "1B":
             pr = 1024000000
             vr = 1023999989
             break
-            
-        if i.upper() == "2048M" or i.upper() == "2B":
+        if i == "2048M" or i == "2B":
             pr = 2048000000
             vr = 2047999957
             break
-            
-        if i.upper() == "4096M" or i.upper() == "4B":
+        if i == "4096M" or i == "4B":
             pr = 4096000000
             vr = 4095999983
             break
-            
-        if i.upper() == "8192M" or i.upper() == "8B":
+        if i == "8192M" or i == "8B":
             pr = 8192000000
             vr = 8191999993
             break
-            
-        if i.upper() == "16B":
+        if i == "16384M" or i == "16B":
             pr = 16384000000
             vr = 16383999977
             break
-            
-        if i.upper() == "32B":
+        if i == "32768M" or i == "32B":
             pr = 32768000000
             vr = 32767999997
-            break            
-            
-        if i.upper() == "-H" or i.upper() == "--HELP" or i.upper() == "HELP":
-            print("Usage:\nPYPrime.exe [32-1024M or 1-32B] [Number of iterations, the default is 7]\n\nBenchmark written by Monabuntur, build 210518")       
-            exit()
-           
-    try:
-        runs = int(sys.argv[2])
-    except ValueError or IndexError:
-        pass
-    
-    
-    
+            break
+
+    if pr == 0:
+        print("Usage: PYPrime.exe [1-1024M, 1-32B]")
+        break
+
     # Header
     qpf = wintypes.LARGE_INTEGER()
 
@@ -271,45 +275,14 @@ while True:
 
     Header = Header(qpf.value)
     Header.output()
-    
-    print("Starting benchmark:\n")
-    
-    for i in range(runs):
-        # Benchmark
-        run = benchmark(pr, qpf.value)            
-            
-        valid = "VALID" if run[1] else "INVALID"
-        # Output end time
-        print(f"Run {i + 1} {valid} ------ Completed in {run[2]} s; Prime: {run[0]:n}")
-        
-        if not run[1]:
-            break
-    
-        results.append(run[2])
-        
-    else:   
-       
-        try:
-            import numpy
-        
-            std = numpy.std(results)
-        
-            for t in results:
-                if t >= numpy.mean(results) - std or t <= numpy.mean(results) + std:
-                    corr_results.append(t)
 
-            Score = Score(numpy.mean(corr_results))
-            Score.output()
-            
-        except ModuleNotFoundError:
-            
-            print("\nPlease install numpy for more precise results\n") 
-            
-            if len(results) >= 5:
-                Score = Score(sum(sorted(results)[1:-1]) / (len(results) - 2))
-                Score.output()
-            
-            else:
-                Score = Score(sum(results) / len(results))
-                Score.output()
+    # Benchmark
+    run = benchmark(pr, qpf.value)
+    #print(f"RUN: {run}")
+
+    # Score
+    Score = Score(run[0], run[1], run[2])
+    Score.output()
+
     break
+    
