156ac548e97a9b3c7f40fb1fc2792b0541331caa
Jaakko <potto558@gmail.com>
2022-07-27 23:15:44 +0200

    PYPrime 2.3, Build 220722

--- PYPrime.pyx
+++ PYPrime.pyx
@@ -1,8 +1,10 @@
-# cython: language_level=3, binding=True, wraparound=False, boundscheck=False, initializedcheck=False, unraisable_tracebacks=False, annotation_typing=False, cdivision=True
+# cython: language_level=3, binding=True, boundscheck=False, initializedcheck=False, unraisable_tracebacks=False, annotation_typing=False, cdivision=True
 
 import sys
+import os
 import math
 import locale
+import subprocess
 
 from cython.view cimport array as cvarray
 from libc.stdio cimport printf
@@ -10,7 +12,7 @@ from libc.stdio cimport printf
 from platform import system, release, version
 from ctypes import WinDLL, wintypes, byref
 
-locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+locale.setlocale(locale.LC_ALL, 'C')
 
 # Types
 ctypedef unsigned long long ull
@@ -21,67 +23,134 @@ kernel32 = WinDLL('kernel32', use_last_error=True)
 # Globals
 cdef ull pr = 0
 cdef ull vr = -1
+runs = 7
+results = []
+corr_results = []
+primes = []
+mode = "M" # Modes can be M (for mean) and B (for best)
+
+pr = 2048000000
+vr = 2047999957
+
+os.system("color")
+
+COLOR = {
+    "GREEN": "\033[92m",
+    "RED": "\033[91m",
+    "ENDC": "\033[0m"
+}
+
+memorytypes = {
+    1: 'Other',
+    2: 'Unknown',
+    20: 'DDR',
+    21: 'DDR2',
+    22: 'DDR2 FB-DIMM',
+    24: 'DDR3',
+    26: 'DDR4',
+    34: 'DDR5',
+    27: 'LPDDR',
+    28: 'LPDDR2',
+    29: 'LPDDR3',
+    30: 'LPDDR4',
+    35: 'LPDDR5'
+}
 
 # Header
 
 class Header:
     def __init__(self, qpf):
-        self.OS = f'{system()} {release()}, Build {version()}'
-        self.qpf = qpf
+        command = "powershell.exe (Get-WMIObject win32_processor).name; " \
+          "(Get-WMIObject win32_operatingsystem).name; " \
+          "(Get-WmiObject Win32_PhysicalMemory Capacity, Speed, TotalWidth, SMBIOSMemoryType)"
+        
+        OSver = subprocess.run(command, shell=True, stdout=subprocess.PIPE, universal_newlines=True).stdout.split('\n')
 
+        self.memspeed = list(filter(lambda a: 'Speed' in a, OSver))[0].split(': ')[1] 
+        self.memcap = sum(int(item.split(':')[1]) for item in list(filter(lambda a: 'Capacity' in a, OSver))) // 1024 // 1024 // 1024
+        self.buswidth = sum(int(item.split(':')[1]) for item in list(filter(lambda a: 'TotalWidth' in a, OSver)))
+        try:
+            self.memtype = memorytypes[int(list(filter(lambda a: 'SMBIOSMemoryType' in a, OSver))[0].split(': ')[1])]
+        except KeyError:
+            self.memtype = 'Unknown'
+        self.OS = OSver[1].split('|')[0].split('\n')[-1]
+        self.CPU = f"{OSver[0]}"
+        self.qpf = qpf
+        
+	
     def output(self):
         hpr = pr // 1000000
         ppr = "M"
         
         if hpr > 1024:
-            hpr //= 1000
+            hpr //= 1024
             ppr = "B"
+            
+        if pr // 8 // 1000000000 > int(self.memcap):
+            if pr // 8 // 1000000 < 1000:
+                memall = f'{COLOR["RED"]}{pr // 8 // 1000000} MB  !!! WARNING !!! Insufficient memory resources{COLOR["ENDC"]}'
+                
+            elif pr // 8 // 1000000 > 1000:
+                memall = f'{COLOR["RED"]}{pr // 8 // 1000000000} GB  !!! WARNING !!! Insufficient memory resources {COLOR["ENDC"]}'
+        else:
+            if pr // 8 // 1000000 < 1000:
+                memall = f'{pr // 8 // 1000000} MB'
+                
+            elif pr // 8 // 1000000 > 1000:
+                memall = f'{pr // 8 // 1000000000} GB'
+                
     
-        print(f"{85 * '-'}\n{35 * ' '}PYPrime 2.0{35 * ' '}\n{85 * '-'}\n\n" \
-              f'OS    : {self.OS}\n' \
-              f'Timer : {round(self.qpf / 1000000, 2)} MHz\n' \
-              f'Prime : {hpr}{ppr} - up to {pr:n}\n', flush=True)
+        print(f"{85 * '-'}\n{35 * ' '}PYPrime 2.3 Windows{35 * ' '}\n{85 * '-'}\n\n" 
+              f' OS                 : {self.OS}, Build {version()}\n' 
+              f' CPU                : {self.CPU}\n'
+              f' RAM                : {self.memcap} GB {self.memtype} @ {self.memspeed} MT/s - {self.buswidth} bit\n\n'
+              
+              f' Benchmark Version  : PYPrime 2.3, Build 220722\n' 
+              f' Prime              : {hpr}{ppr} - up to {pr:n}\n'
+              f' Python Version     : Python {sys.version_info[0]}.{sys.version_info[1]}.{sys.version_info[2]}\n'
+              f' Memory Allocation  : {memall}\n'
+              f' Timer              : {round(self.qpf / 1000000, 2)} MHz\n', flush=True) 
+              
+
+# Standard deviation
+
+def calcstd (ls):
+    n = len(ls)
+    mean = sum(ls) / n
+    var = sum((x - mean)**2 for x in ls) / n
+    std_dev = var ** 0.5
+    return std_dev
+    
+# Mean
+
+def calcmean (ls):
+    n = len(ls)
+    mean = sum(ls) / n
+    return mean
+    
 
 # Score
 
 class Score:   
-    def __init__(self, prime, valid, time):
-        self.prime = prime
-        self.valid = valid
+    def __init__(self, time, mode):
         self.time = time
+        self.mode = mode
 
     def output(self):
-        if not self.valid:
-            print(f"\nINVALID: {self.prime}", flush=True)
-        else:
-            line1 = f"Prime number     : {self.prime:n} is VALID"
-            line2 = f"Computation time : {round(self.time, 3):0.3f} s"
-
-            print("\n".join(["", line1, line2]), flush=True)
+            if mode == "M":
+                print(f"\n Average computation time : {round(self.time, 3):0.3f} s\n")
+            else: 
+                print(f"\n Best computation time : {round(self.time, 3):0.3f} s\n")
 
 
 # Benchmark
-
-def print_memalloc(bytes):
-    size = bytes / 1000000000
-    name = "GB"
-
-    if size < 1.0:
-        size = bytes / 1000000
-        name = "MB"
-        
-        if size < 1.0:
-            size = bytes / 1000
-            name = "KB"
-    
-    print("Sieve allocation: {:0.1f} {}".format(round(size, 1), name), flush=True)
 
 cdef print_status(int loop, ull qpf, ull start_time):
     end_time = wintypes.LARGE_INTEGER()
 
     kernel32.QueryPerformanceCounter(byref(end_time))
-
-    print("    Step {:} ....... {:0.3f} s".format(loop, round((end_time.value - start_time) / qpf, 3)), flush=True)
+    
+    print(f" |{'█'* (loop * 2) + ' '* (18 - loop * 2)}| step {loop} --- {round((end_time.value - start_time) / qpf, 3))} s   ", end="\r")
 
 cdef ull calc(unsigned char [::1] sieve, ull limit, ull sqrtlimit, ull qpf, ull start_time) nogil:
     cdef ull limit1, sqrtlimit1, loopstep, nextstep, x, x2, x2b3, x2b4, y, y2, n, m, o, nd, md
@@ -161,20 +230,18 @@ cdef ull calc(unsigned char [::1] sieve, ull limit, ull sqrtlimit, ull qpf, ull 
     return x
 
 cdef benchmark(ull limit, ull qpf):
-    cdef ull resultx, result
-    cdef ull sieve_len = (limit // 8) + 1
 
     start_time = wintypes.LARGE_INTEGER()
     end_time = wintypes.LARGE_INTEGER()
 
-    # Memory allocation
-    print_memalloc(sieve_len)
+    cdef ull resultx, result
+    cdef ull sieve_len = (limit // 8) + 1
 
+    
     sieve_data = cvarray(shape=(sieve_len,), itemsize=sizeof(unsigned char), format="B")
     cdef unsigned char[::1] sieve = sieve_data
 
-    print("Starting benchmark:\n", flush=True)
-
+    
     # Start timestamp
     kernel32.QueryPerformanceCounter(byref(start_time))
 
@@ -188,8 +255,6 @@ cdef benchmark(ull limit, ull qpf):
     result = (sieve[resultx].bit_length() - 1) + resultx * 8
     time = round((end_time.value - start_time.value) / qpf, 3)
 
-    # Output end time
-    print("    Sieve Scan ... {:0.3f} s".format(time), flush=True)
 
     return [ result, result == vr, time ]
 
@@ -197,77 +262,98 @@ cdef benchmark(ull limit, ull qpf):
 # Main
 
 while True:
-    # Command line parameters
-    for i in sys.argv:
-        if i == "1M":
-            pr = 1000000
-            vr = 999983
-            break
-        if i == "2M":
-            pr = 2000000
-            vr = 1999993
-            break
-        if i == "4M":
-            pr = 4000000
-            vr = 3999971
-            break
-        if i == "8M":
-            pr = 8000000
-            vr = 7999993
-            break
-        if i == "16M":
-            pr = 20000000
-            vr = 19999999
-            break
-        if i == "32M":
-            pr = 32000000
-            vr = 31999939
-            break
-        if i == "64M":
-            pr = 64000000
-            vr = 63999979
-            break
-        if i == "128M":
-            pr = 128000000
-            vr = 127999981
-            break
-        if i == "256M":
-            pr = 256000000
-            vr = 255999983
-            break
-        if i == "512M":
-            pr = 512000000
-            vr = 511999979
-            break
-        if i == "1024M" or i == "1B":
-            pr = 1024000000
-            vr = 1023999989
-            break
-        if i == "2048M" or i == "2B":
-            pr = 2048000000
-            vr = 2047999957
-            break
-        if i == "4096M" or i == "4B":
-            pr = 4096000000
-            vr = 4095999983
-            break
-        if i == "8192M" or i == "8B":
-            pr = 8192000000
-            vr = 8191999993
-            break
-        if i == "16384M" or i == "16B":
-            pr = 16384000000
-            vr = 16383999977
-            break
-        if i == "32768M" or i == "32B":
-            pr = 32768000000
-            vr = 32767999997
-            break
+    
+    # to be replaced with case matching
+    if len(sys.argv) != 1:
+        # Command line parameters
+        for i in sys.argv[1:]:
+    
+            if i.upper() == "32M":
+                pr = 32000000
+                vr = 31999939
+                break
+    
+            if i.upper() == "64M":
+                pr = 64000000
+                vr = 63999979
+                break
+    
+            if i.upper() == "128M":
+                pr = 128000000
+                vr = 127999981
+                break
+    
+            if i.upper() == "256M":
+                pr = 256000000
+                vr = 255999983
+                break
+    
+            if i.upper() == "512M":
+                pr = 512000000
+                vr = 511999979
+                break
+    
+            if i.upper() == "1024M" or i.upper() == "1B":
+                pr = 1024000000
+                vr = 1023999989
+                break
+    
+            if i.upper() == "2048M" or i.upper() == "2B":
+                pr = 2048000000
+                vr = 2047999957
+                break
+    
+            if i.upper() == "4096M" or i.upper() == "4B":
+                pr = 4096000000
+                vr = 4095999983
+                break
+    
+            if i.upper() == "8192M" or i.upper() == "8B":
+                pr = 8192000000
+                vr = 8191999993 
+                break
+    
+            if i.upper() == "16B":
+                pr = 16384000000
+                vr = 16383999977
+                break
+    
+            if i.upper() == "32B":
+                pr = 32768000000
+                vr = 32767999997
+                break
+            
+            if i.upper() == "64B":
+                pr = 65536000000
+                vr = 65535999941
+                break
+                
+            else:
+                print("Usage:\nPYPrime.exe [32-1024M or 1-64B] [Number of iterations, the default is 7] [Mode, M for mean, B for best]\n\nBenchmark written by Monabuntur, build 220722")
+                sys.exit()
 
-    if pr == 0:
-        print("Usage: PYPrime.exe [1-1024M, 1-32B]")
-        break
-
+        if len(sys.argv) == 2:
+            runs = 7
+    
+        if len(sys.argv) == 3:
+            try:
+                runs = int(sys.argv[2])
+    
+            except ValueError or IndexError:
+                print("Usage:\nPYPrime.exe [32-1024M or 1-64B] [Number of iterations, the default is 7] [Mode, M for mean, B for best]\n\nBenchmark written by Monabuntur, build 220722")
+                sys.exit()
+                
+        if len(sys.argv) == 4:
+            mode = (sys.argv[3]).upper()
+            
+            try:
+                runs = int(sys.argv[2])
+    
+            except ValueError or IndexError:
+                print("Usage:\nPYPrime.exe [32-1024M or 1-64B] [Number of iterations, the default is 7] [Mode, M for mean, B for best]\n\nBenchmark written by Monabuntur, build 220722")
+                sys.exit()
+    
+    
     # Header
     qpf = wintypes.LARGE_INTEGER()
 
@@ -275,13 +361,45 @@ while True:
 
     Header = Header(qpf.value)
     Header.output()
+    
+    input("Press ENTER to start the benchmark:")
+    print(" ") 
+        
+    for i in range(runs):
+        # Benchmark
+        run = benchmark(pr, qpf.value)            
+            
+        valid = ("GREEN", "VALID") if run[1] else ("RED", "INVALID")
+        # Output end time
+        print(f' Run {i + 1} {COLOR[valid[0]]} {valid[1]} {COLOR["ENDC"]} ------ Completed in {format(run[2], ".3f")} s; Prime: {run[0]:n}')
+        
+        if not run[1]:
+            break
+    
+        results.append(run[2])
+        
+    else:   
+        if mode == "M":        
+            std = calcstd(results)
+            mean = calcmean(results)
+            for t in results:
+                if t >= mean - std and t <= mean + std:
+                    corr_results.append(t)
 
-    # Benchmark
-    run = benchmark(pr, qpf.value)
-    #print(f"RUN: {run}")
-
-    # Score
-    Score = Score(run[0], run[1], run[2])
-    Score.output()
-
+            Score = Score(calcmean(corr_results), mode)
+            Score.output()
+            
+				
+        elif mode == "B":
+            results.sort()
+            Score = Score(results[0], mode)
+            Score.output()
+                
+        else:
+            results.sort()
+            Score = Score(results[0], mode)
+            Score.output()
+            
+    input("Press ENTER to exit")
+    
     break
